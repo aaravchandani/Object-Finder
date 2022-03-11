f121baa2a9acf34566c62a7d75109ec74a4f934f
aaravchandani <81931219+aaravchandani@users.noreply.github.com>
2022-03-10 18:44:10 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 video = "" 
 
 function preload(){
-    video = createVideo('video.mp4')
-    video.hide()
+   
 }
 function setup(){
     canvas = createCanvas(480, 380);
     canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
 }
 function draw(){
     image(video, 0, 0, 480, 380);
